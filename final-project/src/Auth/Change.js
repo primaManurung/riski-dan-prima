39793ca5911f2bf52f523b43635a408a3e5a9525
dnriski <nugrohoriski159@gmail.com>
2022-09-20 15:07:58 +0700

    update again

--- final-project/src/Auth/Change.js
+++ final-project/src/Auth/Change.js
@@ -3,11 +3,34 @@ import { Button, Form, Input } from "antd";
 import { UserContext } from "./UserContext";
 import axios from "axios";
 import SideNavMenu from "../Layout/Sidenav";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 const Change = () => {
+  const url = "https://super-bootcamp-backend.sanbersy.com";
   const [user, setUser] = useContext(UserContext);
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
+  const [fetchTrigger, setFetchTrigger] = useState(true);
+  const [name, setInputName] = useState("");
+  const [email, setInputEmail] = useState("");
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await axios.get(`${url}/api/change-password/`);
+  //     const currentScore = result.data;
+  //     // setCurrentId(id);
+  //     setInputName(currentScore.name);
+  //     setInputEmail(currentScore.email);
+  //     setFetchTrigger(false);
+  //   };
+  //   if (fetchTrigger) {
+  //     fetchData();
+  //   }
+  // }, [fetchTrigger]);
+
+  const test = () => {
+    alert(name);
+  };
 
   const inputPassword = (event) => {
     let setValue = event.target.value;
@@ -24,12 +47,13 @@ const Change = () => {
       alert("Password Tidak sama");
     } else if (password === password2) {
       alert("Password sama");
+      axios.put(`${url}/api/change-password`);
     }
-    user.password(password);
   };
 
   return (
     <>
+      <button onClick={test}>submit</button>
       <div className={user ? `MainContainerUser1` : `MainContainer`}>
         <div className={user ? `SideContainerUser` : `SideContainer`}>
           {user ? <SideNavMenu /> : <></>}
@@ -41,6 +65,7 @@ const Change = () => {
             <div>
               <h1>{user.email}</h1>{" "}
             </div>
+
             <div>
               password baru :<input onChange={inputPassword}></input>
             </div>
